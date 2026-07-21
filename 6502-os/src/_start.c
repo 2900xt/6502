689e4b8c85c8b74662a2dabe32ea150b1b3bac68
Taha Rawjani <tahakazim08@gmail.com>
2026-07-21 01:27:12 -0400

    working on i2c

--- 6502-os/src/_start.c
+++ 6502-os/src/_start.c
@@ -1,7 +1,12 @@
 #include <types.h>
 
-void _start(void)
-{
-    // Halt
-    for(;;);
+void _start(void) {
+
+  volatile byte_t *PORTB = (volatile byte_t *)0x6000;
+  volatile byte_t *DDRB = (volatile byte_t *)0x6002;
+  *DDRB = 0xFF;
+  *PORTB = 0xFF;
+  // Halt
+  for (;;)
+    ;
 }
